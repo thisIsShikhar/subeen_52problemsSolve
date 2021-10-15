01ac93fdaf5944a8982d7768126df6de1d4398c4
thisIsShikhar <shikharali13@gmail.com>
2021-10-15 17:49:37 +0600

    Problem 01

--- Problem 01.py
+++ Problem 01.py
@@ -2,6 +2,7 @@ a= int(input())
 
 for b in range(0,a):
     c=int(input())
-    if c%2==0:
-        print('even')
-    else: print("odd")
+    if c>=0 and c<=2147483647:
+        if c%2==0:
+            print('even')
+        else: print("odd")
