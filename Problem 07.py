6d6e85f7dfbbd3acf35d6a38357cfa30bb917845
shikhar <shikhar@newroztech.com>
2021-11-09 14:55:51 +0600

    Problem 07

--- Problem 07.py
+++ Problem 07.py
@@ -2,5 +2,14 @@ x=int(input())
 if x>=1 and x<=100:
     for y in range(0,x):
         a=input().split(' ')
-        print(len(a))
+        z=[]
+        for i in a:
+            try:
+                int(i)
+                z.append(i)
+            except:
+                continue
         
+        if len(z)>=1 and len(z)<=100 and sum(int(j) for j in z)<=10000000:
+            print(z)
+            print(len(z))
