b7277d6550b3c9fa27b53ba556709d902d2cf0d3
thisIsShikhar <shikharali13@gmail.com>
2021-11-13 22:41:02 +0600

    Problem 15

---
+++ Problem 15.py
@@ -0,0 +1,12 @@
+x=int(input())
+v=0
+if x>=1 and x<=100:
+    for y in range(0,x):
+        a = input()
+
+        if len(a)<=1000:
+            c = [x for x in a]
+            m = dict((i, a.count(i)) for i in a)
+            for k, v in m.items():
+                print(f"{k} = {v}")
+
