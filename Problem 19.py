41b6c6da9c5fb98c73f607dbaa5687b7d005a0fe
shikhar <shikharali13@gmail.com>
2021-11-15 12:07:21 +0600

    Problem 19

--- Problem 19.py
+++ Problem 19.py
@@ -2,9 +2,9 @@ x=int(input())
 v=0
 if x>=1 and x<=100:
     for y in range(0,x):
-        a = input().split(' ')
+        S = input().split(' ')
         z=0
-        if (len(a))<=10000:
-            for i in a:
+        if (len(S))<=10000:
+            for i in S:
                 z+=1
         print(f"Count = {z}")
