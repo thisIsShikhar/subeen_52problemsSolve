b22b79eee59fc952612e8ed4b6b78c39935ee352
shikhar <shikharali13@gmail.com>
2021-11-15 11:56:26 +0600

    Problem 18

---
+++ Problem 18.py
@@ -0,0 +1,15 @@
+x=int(input())
+v=0
+if x>=1 and x<=100:
+    for y in range(0,x):
+        a = input().lower()
+        z=[]
+        y=[]
+        for i in a:
+            if i == " ":
+                continue
+            elif i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+                z.append(i)
+            else: y.append(i)
+        print(*z,sep='')
+        print(*y,sep='')
