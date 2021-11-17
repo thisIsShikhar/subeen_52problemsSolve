c030d908cee8da003c96ababfcf3be59c23322bb
thisIsShikhar <shikharali13@gmail.com>
2021-11-18 02:11:55 +0600

    Problem 27

---
+++ Problem 27.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+if 1 <= T <= 1000:
+    for i in range(T):
+        n = int(input())
+        if 100<=n<=999:
+            b= 0
+            for j in str(n):
+                b+=int(j)**3
+            if b == n:
+                print(f"{n} is an armstrong number!")
+            else:
+                print(f"{n} is not an armstrong number!")
