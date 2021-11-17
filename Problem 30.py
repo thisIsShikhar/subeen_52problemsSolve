60a97dd0abec2829d693d7c38753ac3e22560b81
thisIsShikhar <shikharali13@gmail.com>
2021-11-18 03:01:25 +0600

    Problem 30

--- Problem 30.py
+++ Problem 30.py
@@ -4,11 +4,12 @@ if 1 <= T <= 100:
     for i in range(T):
         N = int(input())
         a=[]
-        for x in range(1,N):
-            if N%x==0:
-                a.append(x)
-            else: continue
-        if sum(a)==N:
-            print(f"YES, {N} is a perfect number!")
-        else:
-            print(f"No, {N} is not a perfect number!")
+        if N<=(2**64)-1:
+            for x in range(1,N):
+                if N%x==0:
+                    a.append(x)
+                else: continue
+            if sum(a)==N:
+                print(f"YES, {N} is a perfect number!")
+            else:
+                print(f"No, {N} is not a perfect number!")
