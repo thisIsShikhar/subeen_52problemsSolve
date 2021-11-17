af70b046afbd9dc1b95434374b67de4469b51322
thisIsShikhar <shikharali13@gmail.com>
2021-11-18 02:50:19 +0600

    Problem 29

---
+++ Problem 29.py
@@ -0,0 +1,15 @@
+import string
+
+T = int(input())
+
+if 1 <= T <= 100:
+    for i in range(T):
+        n = (input())
+        if n.isupper():
+            print("Uppercase Character")
+        elif n.islower():
+            print("Lowercase Character")
+        elif n.isdigit():
+            print("Numeric Digit")
+        else:
+            print("Special Character")
