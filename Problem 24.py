b9f0e040e6b01875fd1e41b687c0a14f35ee3008
thisIsShikhar <shikharali13@gmail.com>
2021-11-18 01:27:11 +0600

    Problem 24

---
+++ Problem 24.py
@@ -0,0 +1,9 @@
+T = int(input())
+if 1 <= T <= 100:
+    for i in range(x):
+        array_element_number = int(input())
+        n = []
+        for m in range(array_element_number):
+            b = int(input())
+            n.append(b)
+        print(n[::2])
