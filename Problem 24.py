e6137ec6e59b5c9281163fe2c26c2e3e8a51bb30
thisIsShikhar <shikharali13@gmail.com>
2021-11-18 01:41:13 +0600

    Problem 24

--- Problem 24.py
+++ Problem 24.py
@@ -1,6 +1,6 @@
 T = int(input())
 if 1 <= T <= 100:
-    for i in range(x):
+    for i in range(T):
         array_element_number = int(input())
         n = []
         for m in range(array_element_number):
