bce7003ff1f1674c3772cc1cfbca112836e2eb86
shikhar <shikharali13@gmail.com>
2021-12-22 23:27:38 +0600

    Problem 52

--- Problem 52.py
+++ Problem 52.py
@@ -1,12 +1,19 @@
+T = int(input())
 
-string = input()
-sub = input()
+if 1 <= T <= 100:
+    for y in range(0, T):
+        a = input()
+        b = input()
+        if len(a) < 128 and len(b) < 128:
+            def to_find(string, sub):
+                count = 0
+                start = 0
+                while True:
+                    start = string.find(sub, start) + 1
+                    if start > 0:
+                        count += 1
+                    else:
+                        return count
 
-count = start = 0
-while True:
-    start = string.find(sub, start) + 1
-    if start > 0:
-        count+=1
-    else:
-        continue
-print(count)
+
+            print(to_find(a, b))
