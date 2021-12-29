0bffe407f01731bcace32fd119ce825f1e1a71cb
shikhar <shikharali13@gmail.com>
2021-12-29 23:33:28 +0600

    Problem 42.py

---
+++ Problem 13.py
@@ -0,0 +1,14 @@
+# T = int(input())
+# if T <= 10:
+#     for i in range(1, T + 1):
+
+a= "no one is good and no one is bad"
+z=a.split()
+c=[]
+for i in z:
+    print(i)
+
+    b=a.count(i)
+    c.append(b)
+print(c)
+
