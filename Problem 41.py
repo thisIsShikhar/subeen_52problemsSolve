e4ed9b920517516180ce888fa57c7f58d7639ee9
shikhar <shikharali13@gmail.com>
2021-12-30 18:28:37 +0600

    Problem 41

--- Problem 41.py
+++ Problem 41.py
@@ -1,6 +1,13 @@
+import math
+
 T = int(input())
 
 if T <= 100:
     for i in range(T):
         n = int(input())
-        if n<=15:
+        if n <= 15:
+            a = 0
+            for j in range(1, n + 1):
+                a += (j / math.factorial(j))
+
+            print("{:.4f}".format(a))
