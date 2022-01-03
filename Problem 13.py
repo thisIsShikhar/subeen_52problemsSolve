a55a0b7b53881fe5198404799930ee7314d9e04e
shikhar <shikharali13@gmail.com>
2022-01-03 17:55:29 +0600

    Problem 13

--- Problem 13.py
+++ Problem 13.py
@@ -1,14 +1,20 @@
-# T = int(input())
-# if T <= 10:
-#     for i in range(1, T + 1):
+import math
 
-a= "no one is good and no one is bad"
-z=a.split()
-c=[]
-for i in z:
-    print(i)
+T = int(input())
+if 1 <= T <= 100:
+    for i in range(1, T + 1):
+        a = input()
+        if len(a) <= 20:
+            z = a.split()
+            c = []
+            d = 1
+            for i in z:
+                if i in c:
+                    continue
+                else:
+                    count = z.count(i)
+                    if count >= 2:
+                        c.append(i)
+                        d *= count
 
-    b=a.count(i)
-    c.append(b)
-print(c)
-
+            print(f"1/{int(math.factorial(len(z))/d)}")
